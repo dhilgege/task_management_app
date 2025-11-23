2820f51f2ca68a10e3db6803d584ad590c2ef953
fadil <dhilgege@gmail.com>
2025-11-23 19:57:56 +0700

    video 1dan2| flutter task managment app, 02 Flutter Task Management App | Create Project, Create Responsive Login Page / Screen

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Container(
+        margin: context.isPhone
+            ? EdgeInsets.all(Get.width * 0.1)
+            : EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            !context.isPhone
+                ? Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          bottomLeft: Radius.circular(50),
+                        ),
+                        color: Colors.blue,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Welcome",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 70,
+                              ),
+                            ),
+                            Text(
+                              "please sign in",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                              ),
+                            ),
+                            Text(
+                              "start journey with us",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : const SizedBox(),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    context.isPhone
+                        ? Column(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Welcome",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 40,
+                                ),
+                              ),
+                              Text(
+                                "please sign in",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text(
+                                "start journey with us",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          )
+                        : const SizedBox(),
+                    Image.asset(
+                      "assets/images/login.png",
+                      height: Get.height * 0.5,
+                    ),
+                    FloatingActionButton.extended(
+                      onPressed: () => Get.toNamed(Routes.HOME),
+                      backgroundColor: Colors.blue,
+                      label: const Text("sign in with google"),
+                      icon: const Icon(
+                        Ionicons.logo_google,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
